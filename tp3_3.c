b8ee26bf76774c6bce25c6efec0d33a25759b378
Thomas Bertoluzzi <bertoluzzit2@gmail.com>
2023-04-03 17:05:28 -0300

    Punto 3

---
+++ tp3_3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define STRMAX 50
+
+int main()
+{
+    char *arreglo[5];
+    for (int i = 0; i < 5; i++)
+    {
+        arreglo[i] = (char*) malloc(STRMAX * sizeof(char*));
+        printf("Ingrese un nombre: ");
+        fflush(stdin);
+        gets(arreglo[i]);
+        printf("Nombre ingresado: ");
+        puts(arreglo[i]);
+        printf("\n");
+    }
+    
+}
